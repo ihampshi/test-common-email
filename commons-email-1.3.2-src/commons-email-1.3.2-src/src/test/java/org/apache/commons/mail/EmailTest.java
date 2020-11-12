77d3a6eafe42c9522f5c6998b7d9c91f799a3097
ihampshi <46584868+ihampshi@users.noreply.github.com>
2020-11-12 13:31:20 -0500

    Testing: addBcc
    Tests added for "addBcc"
    -Testing for array insertion
    -Testing for individual insertion
    -Testing for empty insertion

--- commons-email-1.3.2-src/commons-email-1.3.2-src/src/test/java/org/apache/commons/mail/EmailTest.java
+++ commons-email-1.3.2-src/commons-email-1.3.2-src/src/test/java/org/apache/commons/mail/EmailTest.java
@@ -1,10 +1,16 @@
 package org.apache.commons.mail;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 public class EmailTest {
 
+	private static final String[] TEST_EMAILS = { "qwertyuiop@xyz.com", "mnbqwe@troll.net", "qazxswedc@poiuy.org",
+	"asd123@asdf.com.bd"};
+	
 	//Concrete email instance for testing
 	private EmailConcrete email;
 	
@@ -20,5 +26,29 @@ public class EmailTest {
 	public void tearDownEmailTest() throws Exception {
 		
 		
+	}
+	
+	@Test
+	public void testAddBcc() throws Exception {
+		
+		email.addBcc(TEST_EMAILS);
+		
+		assertEquals(4, email.getBccAddresses().size());
+	}
+	
+	@Test
+	public void test2AddBcc() throws Exception {
+		
+		email.addBcc(TEST_EMAILS[0]);
+		
+		assertEquals(1, email.getBccAddresses().size());
+	}
+	
+	@Test(expected = EmailException.class)
+	public void test3AddBcc() throws Exception {
+		
+		String[] empty = {};
+		
+		email.addBcc(empty);
 	}
 }
