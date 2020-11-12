0423f2e00ced32a59d86fb3e34dd125c4c969fe6
ihampshi <46584868+ihampshi@users.noreply.github.com>
2020-11-12 13:56:00 -0500

    Testing "buildMimeMessage"
    Tests added for "buildMimeMessage"
    -Basic operation test (assert receiver address)
    -Testing reply feature
    -Testing bcc feature
    -Testing cc feature
    -Testing multipart content
    -Testing bodypart content
    -Testing empty content
    -Test for missing sender
    -Test for missing receiver
    -Testing header feature

--- commons-email-1.3.2-src/commons-email-1.3.2-src/src/test/java/org/apache/commons/mail/EmailTest.java
+++ commons-email-1.3.2-src/commons-email-1.3.2-src/src/test/java/org/apache/commons/mail/EmailTest.java
@@ -2,6 +2,12 @@ package org.apache.commons.mail;
 
 import static org.junit.Assert.assertEquals;
 
+import javax.mail.Address;
+import javax.mail.Message.RecipientType;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -137,5 +143,183 @@ public class EmailTest {
 		email.addTo(TEST_EMAILS[0]);
 		
 		assertEquals(1, email.toList.size());
+	}
+	
+	@Test
+	public void testBuildMimeMessage() throws Exception {
+		
+		email.setHostName("localhost");
+		email.setSmtpPort(8080);
+		email.setFrom("a@b.com");
+		email.addTo("c@d.com");
+		email.setSubject("test mail");
+		
+		email.buildMimeMessage();
+		
+		MimeMessage msg = email.getMimeMessage();
+		Address[] strform = msg.getFrom();
+		
+		assertEquals(strform[0].toString(), "a@b.com");
+	}
+	
+	@Test
+	public void test2BuildMimeMessage() throws Exception {
+		
+		email.setHostName("localhost");
+		email.setSmtpPort(8080);
+		email.setFrom("a@b.com");
+		email.addTo("c@d.com");
+		email.setSubject("test mail");
+		
+		email.addReplyTo(TEST_EMAILS[2]);
+		
+		email.buildMimeMessage();
+		
+		MimeMessage msg = email.getMimeMessage();
+		Address[] strform = msg.getReplyTo();
+		
+		assertEquals(strform[0].toString(), TEST_EMAILS[2]);
+	}
+	
+	@Test
+	public void test3BuildMimeMessage() throws Exception {
+		
+		email.setHostName("localhost");
+		email.setSmtpPort(8080);
+		email.setFrom("a@b.com");
+		email.addTo("c@d.com");
+		email.setSubject("test mail");
+		
+		email.addBcc(TEST_EMAILS[0]);
+		email.addCc(TEST_EMAILS[1]);
+		
+		email.buildMimeMessage();
+		
+		MimeMessage msg = email.getMimeMessage();
+		Address[] strform = msg.getRecipients(RecipientType.BCC);
+		
+		assertEquals(strform[0].toString(), TEST_EMAILS[0]);
+	}
+	
+	@Test
+	public void test4BuildMimeMessage() throws Exception {
+		
+		email.setHostName("localhost");
+		email.setSmtpPort(8080);
+		email.setFrom("a@b.com");
+		email.addTo("c@d.com");
+		email.setSubject("test mail");
+		
+		email.addCc(TEST_EMAILS[1]);
+		
+		email.buildMimeMessage();
+		
+		MimeMessage msg = email.getMimeMessage();
+		Address[] strform = msg.getRecipients(RecipientType.CC);
+		
+		assertEquals(strform[0].toString(), TEST_EMAILS[1]);
+	}
+	
+	@Test
+	public void test5BuildMimeMessage() throws Exception {
+		
+		email.setHostName("localhost");
+		email.setSmtpPort(8080);
+		email.setFrom("a@b.com");
+		email.addTo("c@d.com");
+		email.setSubject("test mail");
+		
+		MimeMultipart content = new MimeMultipart();
+		MimeBodyPart bodyPart = new MimeBodyPart();
+		bodyPart.setText("Brought to you by JDK 11.0.9!");
+		content.addBodyPart(bodyPart);
+		
+		email.setContent(content);
+		
+		email.buildMimeMessage();
+		
+		MimeMessage msg = email.getMimeMessage();
+		MimeMultipart returnedContent = (MimeMultipart) msg.getContent();
+		String text = (String) returnedContent.getBodyPart(0).getContent();
+		
+		assertEquals(text, "Brought to you by JDK 11.0.9!");
+	}
+	
+	@Test
+	public void test6BuildMimeMessage() throws Exception {
+		
+		email.setHostName("localhost");
+		email.setSmtpPort(8080);
+		email.setFrom("a@b.com");
+		email.addTo("c@d.com");
+		email.setSubject("test mail");
+		
+		MimeBodyPart body = new MimeBodyPart();
+		body.setText("Brought to you by JDK 11.0.9!");
+		
+		email.setContent(body, "text");
+		
+		email.buildMimeMessage();
+		
+		MimeMessage msg = email.getMimeMessage();
+		MimeBodyPart returnedContent = (MimeBodyPart) msg.getContent();
+		String text = (String) returnedContent.getContent();
+		
+		System.out.println(text);
+		
+		assertEquals(text, "Brought to you by JDK 11.0.9!");
+	}
+	
+	@Test(expected = IllegalStateException.class)
+	public void test7BuildMimeMessage() throws Exception {
+		
+		email.setHostName("localhost");
+		email.setSmtpPort(8080);
+		email.setFrom("a@b.com");
+		email.addTo("c@d.com");
+		email.setSubject("test mail");
+		
+		email.buildMimeMessage();
+		email.buildMimeMessage();
+	}
+	
+	@Test(expected = EmailException.class)
+	public void test8BuildMimeMessage() throws Exception {
+		
+		email.setHostName("localhost");
+		email.setSmtpPort(8080);
+		email.addTo("c@d.com");
+		email.setSubject("test mail");
+		
+		email.buildMimeMessage();
+	}
+	
+	@Test(expected = EmailException.class)
+	public void test9BuildMimeMessage() throws Exception {
+		
+		email.setHostName("localhost");
+		email.setSmtpPort(8080);
+		email.setFrom("a@b.com");
+		email.setSubject("test mail");
+		
+		email.buildMimeMessage();
+	}
+	
+	@Test
+	public void test10BuildMimeMessage() throws Exception {
+		
+		email.setHostName("localhost");
+		email.setSmtpPort(8080);
+		email.setFrom("a@b.com");
+		email.addTo("c@d.com");
+		email.setSubject("test mail");
+		email.addHeader(TEST_NAME, TEST_HEADER_VALUE);
+		
+		email.buildMimeMessage();
+		
+		MimeMessage msg = email.getMimeMessage();
+		String[] header = msg.getHeader(TEST_NAME);
+		
+		assertEquals(TEST_HEADER_VALUE, header[0]);
 	}
 }
