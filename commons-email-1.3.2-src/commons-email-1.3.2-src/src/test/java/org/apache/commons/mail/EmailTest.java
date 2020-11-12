de2076cd400fce8a6697a54f5c6d02d7167eb8ee
ihampshi <46584868+ihampshi@users.noreply.github.com>
2020-11-12 13:29:08 -0500

    Added setup/teardown
    Added methods and initialization and deconstruction for tests in the testing class

--- commons-email-1.3.2-src/commons-email-1.3.2-src/src/test/java/org/apache/commons/mail/EmailTest.java
+++ commons-email-1.3.2-src/commons-email-1.3.2-src/src/test/java/org/apache/commons/mail/EmailTest.java
@@ -1,6 +1,24 @@
 package org.apache.commons.mail;
 
+import org.junit.After;
+import org.junit.Before;
+
 public class EmailTest {
 
+	//Concrete email instance for testing
+	private EmailConcrete email;
 	
+	@Before
+	public void setUpEmailTest() throws Exception {
+		
+		email = new EmailConcrete();
+		
+		
+	}
+	
+	@After
+	public void tearDownEmailTest() throws Exception {
+		
+		
+	}
 }
